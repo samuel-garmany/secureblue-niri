34b401e6c3f627746b94af349ed99bf7010d2e4a
Samuel Garmany <65299214+samuel-garmany@users.noreply.github.com>
2026-09-19 15:06:27 -0600

    fix build issue

--- files/system/etc/bashrc.d/local.sh
+++ files/system/etc/bashrc.d/local.sh
@@ -2,18 +2,19 @@ alias cat=bat
 alias ls=eza
 alias ll="eza -l"
 alias la="eza -la"
+alias vim="nvim"
 
 eval "$(starship init bash)"
 eval "$(zoxide init --cmd cd bash)"
 eval "$(fzf --bash)"
 
 y() {
-	local tmp cwd
-	tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	command yazi "$@" --cwd-file="$tmp"
-	cwd="$(command cat -- "$tmp")"
-	if [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && [ -d "$cwd" ]; then
-		cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp cwd
+  tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+  command yazi "$@" --cwd-file="$tmp"
+  cwd="$(command cat -- "$tmp")"
+  if [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && [ -d "$cwd" ]; then
+    cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
